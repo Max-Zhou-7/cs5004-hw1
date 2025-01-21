dea27cf219c245a3db49a5eae7400cd328e2a130
Zilu Zhou <ziluzhou7@gmail.com>
2025-01-21 15:05:23 -0500

    first commit

---
+++ src/ElectricVehicle.java
@@ -0,0 +1,142 @@
+import java.text.DecimalFormat;
+
+/**
+ * create the class of EV.
+ */
+public class ElectricVehicle {
+    private String name;
+    private double batterySize;
+    private double stateOfCharge;
+    private double currentEfficiency;
+    private double defaultEfficiency;
+
+  /**
+   * default constructor.
+   * @param batterySize batterySize
+   * @param stateOfCharge state of charge
+   * @param defaultEfficiency default efficiency
+   */
+  public ElectricVehicle(double batterySize, double stateOfCharge, double defaultEfficiency) {
+    this.name = "unknown EV";
+    this.batterySize = batterySize;
+    this.stateOfCharge = stateOfCharge;
+    this.defaultEfficiency = defaultEfficiency;
+}
+
+  /**
+   * customize constructor.
+   * @param name name
+   * @param batterySize battery size
+   * @param stateOfCharge state of charge
+   * @param defaultEfficiency default efficiency
+   */
+  public ElectricVehicle(String name, double batterySize, double stateOfCharge, double defaultEfficiency) {
+    this.name = name;
+    this.batterySize = batterySize;
+    this.stateOfCharge = stateOfCharge;
+    this.defaultEfficiency = defaultEfficiency;
+  }
+
+  /**
+   * get the battery size.
+   * @return battery size.
+   */
+  public double getBatterySize(){
+      if(this.batterySize < 10) {
+        this.batterySize = 10;
+      } else if (this.batterySize > 150) {
+        this.batterySize = 150;
+      }
+    return this.batterySize;
+
+  }
+
+  /**
+   * get default efficiency.
+   * @return default efficiency.
+   */
+  public double getDefaultEfficiency() {
+    if (this.defaultEfficiency < 0.5) {
+      this.defaultEfficiency = 0.5;
+    } else if (this.defaultEfficiency > 4.5) {
+      this.defaultEfficiency = 4.5;
+    }
+    return this.defaultEfficiency;
+  }
+  /**
+   * Get name.
+   * @return name.
+   */
+  public String getName() {
+    return this.name;
+  }
+
+  /**
+   * Get current state of charge.
+   * @return state of charge.
+   */
+  public double getStateOfCharge() {
+    if (this.stateOfCharge > 1.0) {
+      this.stateOfCharge = 1.0;
+    } else if (this.stateOfCharge < 0.15) {
+      this.stateOfCharge = 0.15;
+    }
+    return  this.stateOfCharge;
+    }
+
+  /**
+   * Get current efficiency.
+   * return current efficiency
+   */
+  public double getEfficiency() {
+    return this.currentEfficiency;
+    }
+
+  /**
+   * set new value for state of charge.
+   */
+  public void setStateOfCharge(double stateOfCharge) {
+    this.stateOfCharge = stateOfCharge;
+    }
+
+  /**
+   * get the range for car can drive.
+    * @return range.
+   */
+  public double range() {
+    return getEfficiency() * getStateOfCharge() * getBatterySize();
+    }
+
+  /**
+   * get new efficiency based on current temperature.
+   * @param currentTemp currentTemp.
+   */
+  public void updateEfficiency(double currentTemp) {
+      if (currentTemp > 77.0) {
+        this.currentEfficiency = 0.85;
+      }
+      else if(currentTemp < 65.0) {
+        if(currentTemp < 15.0) {
+          this.currentEfficiency = 0.5;
+        }
+        else{
+          double diff = (65 - currentTemp) / 100;
+        this.currentEfficiency = 1 - diff;
+        }
+      }
+
+      }
+
+  /**
+   * Override string
+   * @return string
+   */
+  public String toString() {
+    DecimalFormat percentFormat = new DecimalFormat("0.0%");
+    DecimalFormat rangeFormat = new DecimalFormat("0.0");
+    return this.name + " SOC: " + percentFormat.format(getStateOfCharge()) +
+            " Range (miles): " + rangeFormat.format(range());
+  }
+}
+
+
---
+++ test/testElectricVehicle.java
@@ -0,0 +1,102 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * Beginning of test class.
+ */
+public class testElectricVehicle {
+  ElectricVehicle a;
+  ElectricVehicle b;
+  private final double delta = 0.01;
+/**
+ * set up for car a and b.
+ */
+@Before
+public void setup() {
+  a = new ElectricVehicle(130.734534534, 0.07, 0.3);
+  b = new ElectricVehicle("Xiaomi SU7", 10000.999, 100.99,4.5);
+}
+
+/**
+ * Test get car name.
+ */
+@Test
+public void getName() {
+  assertEquals("unknown EV", a.getName());
+  assertEquals("Xiaomi SU7", b.getName());
+}
+
+/**
+ * Test get Battery size.
+ */
+@Test
+public void testBatterySize() {
+  assertEquals(130.73, a.getBatterySize(), delta);
+  assertEquals(150.0, b.getBatterySize(), delta);
+}
+
+/**
+ * Test get default efficiency.
+ */
+@Test
+public void testGetDefaultEfficiency() {
+  assertEquals(0.5, a.getDefaultEfficiency(),0);
+  assertEquals(4.5, b.getDefaultEfficiency(),0);
+}
+
+/**
+ * Test the set and get state of charge.
+ */
+@Test
+public void testGetStateOfCharge() {
+  assertEquals(0.15, a.getStateOfCharge(), delta);
+  assertEquals(1, b.getStateOfCharge(), delta);
+  a.setStateOfCharge(0.856);
+  b.setStateOfCharge(0.0723);
+  assertEquals(0.86, a.getStateOfCharge(), delta);
+  assertEquals(0.15, b.getStateOfCharge(), delta);
+}
+
+/**
+ * Test get current efficiency.
+ */
+@Test
+public void testGetEfficiency() {
+  a.updateEfficiency(79);
+  b.updateEfficiency(64.1234234);
+  assertEquals(0.85, a.getEfficiency(), delta);
+  assertEquals(0.99, b.getEfficiency(), delta);
+}
+
+/**
+ * Test Range.
+ */
+@Test
+public void testRange() {
+  assertEquals(a.getEfficiency() * a.getStateOfCharge() * a.getBatterySize(), a.range(), delta);
+  assertEquals(b.getEfficiency() * b.getStateOfCharge() * b.getBatterySize(), b.range(), delta);
+}
+
+
+/**
+ * test get toString.
+ */
+@Test
+public void testString() {
+  a.setStateOfCharge(0.856);
+  b.setStateOfCharge(0.0723);
+  a.updateEfficiency(79);
+  b.updateEfficiency(64.1234234);
+  assertEquals("unknown EV SOC: 85.6% Range (miles): 95.1", a.toString());
+  assertEquals("Xiaomi SU7 SOC: 15.0% Range (miles): 22.3", b.toString());
+}
+
+
+
+
+
+
+}
